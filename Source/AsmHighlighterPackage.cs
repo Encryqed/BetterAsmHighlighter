e87fee84687469def982ea7aaa768845b2612244
Encryqed <67229555+Encryqed@users.noreply.github.com>
2026-02-28 17:30:09 +0100

    Added Project Files

---
+++ Source/AsmHighlighterPackage.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace BetterAsmHighlighter
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [ProvideAutoLoad(Microsoft.VisualStudio.Shell.Interop.UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
+    [Guid("f47ac10b-58cc-4372-a567-0e02b2c3d479")]
+    internal sealed class AsmHighlighterPackage : AsyncPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken CancellationToken, IProgress<ServiceProgressData> Progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(CancellationToken);
+            MessageBox.Show("BetterAsmHighlighter loaded!", "BetterAsmHighlighter", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
